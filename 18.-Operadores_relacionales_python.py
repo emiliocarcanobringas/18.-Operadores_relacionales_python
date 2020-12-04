381fb23c4fc30b98d6a15699e9be6b7dfa67bbd6
Emilio Carcaño Bringas <emiliocarcano@uninaciones.edu.mx>
2020-12-03 20:28:37 -0600

    Add files via upload

---
+++ 18.-Operadores_relacionales_python.py
@@ -0,0 +1,26 @@
+'''
+Este programa muestra el uso de los operadores relacionales. Cabe aclarar que los operadores
+aritméticos tienen mayor prioridad que los relacionales.
+'''
+
+comparacion1 = 5 >300 # Planteamos una comparación cuyo valor es falso
+print(comparacion1) # Imprimimos el valor de comparacion1
+
+igualdad1 = 8 == 8 # Mostramos una igualdad cuyo valor es verdadero
+print(igualdad1) # Imprimimos el valor de igualdad1
+
+diferente = 9 != 10 # Planteamos una diferencia cuyo valor es verdadero
+print(diferente) # Imprimimos el valor de diferente
+
+menorOigual = 4 <= 12 #Se propone una comparación o igualdad, se cumple la comparación con valor verdadero
+print (menorOigual) # Imprimimos en pantalla el valor de menorOigual
+
+mayorOigual = 32 >= 32 # Aquí se cumple la igualdad
+print (mayorOigual) # Imprimimos el valor de mayorOigual
+
+numero1 = 8 # Asignamos un valor a numero1
+numero2 = 10 # Asignamos un valor a numero2
+numero3 = 20 # Asignamos un valor a numero3
+
+comparacion3 = numero1+numero2 >= numero3 # comparacion3 realiza una comparación entre la suma de 2 variables y una tercera
+print(comparacion3) # Imprimimos comparacion3 en pantalla
